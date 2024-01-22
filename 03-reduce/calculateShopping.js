edaa54c81bd9a105fa8b77aa073ac0aa4a4557c4
Enma Ormaza <enma.ormaza@mataro.epiaedu.cat>
2024-01-22 19:04:08 +0100

    FORMAT CODE

--- 03-reduce/calculateShopping.js
+++ 03-reduce/calculateShopping.js
@@ -5,24 +5,20 @@
  */
 
 const WISHES = [
-    { title: "Tesla Model S", price: 90000 },
-    { title: "4 carat diamond ring", price: 45000 },
-    { title: "Fancy hacky Sack", price: 5 },
-    { title: "Gold fidgit spinner", price: 2000 },
-    { title: "A second Tesla Model S", price: 90000 },
+  { title: "Tesla Model S", price: 90000 },
+  { title: "4 carat diamond ring", price: 45000 },
+  { title: "Fancy hacky Sack", price: 5 },
+  { title: "Gold fidgit spinner", price: 2000 },
+  { title: "A second Tesla Model S", price: 90000 },
 ];
 
-const calculateShopping = (wishes)=>{
+const calculateShopping = (wishes) => {
+  const totalSum = wishes.reduce((sum, wish) => {
+    const total = sum + wish.price;
+    return total;
+  }, 0);
 
-    const totalSum = wishes.reduce((sum, wish) =>{
-
-        const total = sum + wish.price;
-        return total;
-
-    }, 0);
-
-    return totalSum;
-
-}
+  return totalSum;
+};
 
 export default calculateShopping;
